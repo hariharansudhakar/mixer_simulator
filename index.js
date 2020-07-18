f2e7a8a158d2ebff92ce6fb45152535ead101507
hariharansudhakar <65080586+hariharansudhakar@users.noreply.github.com>
2020-07-18 15:20:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var x = document.getElementById("need1");
+var y = document.getElementById("need2");
+var a=10;
+var b=10;
+var vol=0;
+var vol = x+y;
+
+function vol_upd(){
+  var x = Number(document.getElementById("need1").value);
+  var y = Number(document.getElementById("need2").value);
+  var vol = x+y;
+  document.getElementById("l3").innerHTML = vol+" litres";
+  document.getElementById("l1").innerHTML = a-x+" litres";
+  document.getElementById("l2").innerHTML = b-y+" litres";
+
+  a=a-x;
+  b=b-y;
+
+
+}
+
+function empty(){
+  document.getElementById("l3").innerHTML = 0+" litres";
+}
+
+function fill_tanks(){
+  document.getElementById("l1").innerHTML = 10+"litres";
+  document.getElementById("l2").innerHTML = 10+"litres";
+  a=10;
+  b=10;
+
+}
